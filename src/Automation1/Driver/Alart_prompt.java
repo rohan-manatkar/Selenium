a63686c390092f8c53c2de19590c73c0c2f1d56b
Rohan Manatkar <Rohan Manatkar@LAPTOP-8NB4MEPP>
2024-05-27 16:15:10 +0530

    alart_menu

---
+++ src/Automation1/Driver/Alart_prompt.java
@@ -0,0 +1,18 @@
+package Automation1.Driver;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+public class Alart_prompt {
+public static void main(String[] args) throws InterruptedException {
+	System.setProperty("WebDriver.edge.driver","Driver//msedgedriver.exe");
+	WebDriver driver = new EdgeDriver();
+	String url = "https://demoqa.com/alerts";
+	driver.get(url);
+	driver.findElement(By.xpath("//*[@id=\"promtButton\"]")).click();
+	Alert a=  driver.switchTo().alert();
+	a.sendKeys("hiiiii rohan");
+	//a.accept();
+	Thread.sleep(500);
+}
+}
