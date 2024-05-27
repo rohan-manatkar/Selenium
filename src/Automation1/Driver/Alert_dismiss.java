0736d809f5d134c63ccb55c8685d1924c8a9629e
Rohan Manatkar <Rohan Manatkar@LAPTOP-8NB4MEPP>
2024-05-27 16:20:40 +0530

    first

---
+++ src/Automation1/Driver/Alert_dismiss.java
@@ -0,0 +1,16 @@
+package Automation1.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+public class Alert_dismiss {
+public static void main(String[] args) throws InterruptedException {
+	System.setProperty("WebDriver.edge.driver","Driver//msedgedriver.exe");
+	WebDriver driver = new EdgeDriver();
+	String url = "https://demoqa.com/alerts";
+	driver.get(url);
+	driver.findElement(By.id("confirmButton")).click();
+	Thread.sleep(500);
+	driver.switchTo().alert().dismiss();
+	Thread.sleep(500);
+}
+}
